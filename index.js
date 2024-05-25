af764635a2bf33da42c68ab3ef39f20bb16f988b
phamphukhanhh <phamphukhanh339@gmail.com>
2024-05-25 14:30:15 +0700

    update get questions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const fs = require('fs/promises');
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -5,10 +7,17 @@ const port = 3000;
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });  
-
-app.get("/:user", function (req, res) {
-    res.send(req.params.user);
-}); 
+ReadableByteStreamController
+app.get("/question", function(req, res){
+  fs.readFile("database-MT.json" , "utf-8")
+    .then((data) => {
+      let questions = data;
+      res.send(questions);
+    })
+    .catch((error) => {
+    });
+  //res.send("Hello World!");
+})
 
 
 app.listen(port, function () {
