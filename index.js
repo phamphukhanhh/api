3e413d70ef3971fb187f1cf9c9f9c521e52143c4
phamphukhanhh <phamphukhanh339@gmail.com>
2024-05-25 11:27:37 +0700

    brrr

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});  
+
+app.get(":user", function (req, res) {
+    res.send(req.params.user);
+}); 
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
