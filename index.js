36ed87a95cd34cdabac207d9d1b0c3048f9cf820
phamphukhanhh <phamphukhanh339@gmail.com>
2024-05-25 11:40:20 +0700

    vdsgfs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });  
 
-app.get(":user", function (req, res) {
+app.get("/:user", function (req, res) {
     res.send(req.params.user);
 }); 
 
